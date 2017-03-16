798b12807ba4fc7d3b591616e7400f69b8e41764
Live session user <mint@etudiant.oclock.io>
2017-03-16 20:05:45 +0100

    foreach

--- templates/accueil.php
+++ templates/accueil.php
@@ -3,41 +3,16 @@
     <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
     </p>
-    <ul class="diets">
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-    </ul>
-    <ul class="events">
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-    </ul>
 
 
 
 
 
-<main id="diets" class="container">
-    <?php
-    foreach ($diets as $index => $diet) {
-        $link = "diets.php?index"
-        # code...
+    <main id="diets" class="container">
+        <?php
+    <ul>
+ <?php foreach ($diets as $index => $diet) { ?>
+  <li><a href=regimes.php?"<?php $diet['id'] ?>"><?php echo $diet['nomduregime'] ?></a><li>
+<?php endforeach; ?>
+</ul>
     }
-
-
-</main>
-
-<main id="events" class="container">
-    <?php
-    foreach ($diets as $index => $diet) {
-        $link = "diets.php?index"
-        # code...
-    }
-
-
-</main>
