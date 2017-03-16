0bc959b569c207a05df0aca681bffeef5266e401
Live session user <mint@etudiant.oclock.io>
2017-03-16 17:32:42 +0100

    fonctions page d'accueil

---
+++ inc/data.php
@@ -0,0 +1,35 @@
+<?php
+
+$host = "localhost";
+$user = "boutique";
+$pass = "boutique";
+$dbname = "exo_boutique";
+
+try {
+	$db_connect = new PDO("mysql:host=".$host.";dbname=".$dbname.";charset=utf8",$user,$pass);
+}
+catch(PDOException $e) {
+	die('Erreur : '.$e->getMessage());
+}
+
+/* fonction pour afficher les différents régimes */
+
+function getAllDiets() {
+    global $db_connect;
+
+    $sql = "SELECT * FROM diets";
+    $query = $db_connect->query($sql);
+    $diets = $query->fetchAll(PDO::FETCH_ASSOC);
+    return $diets;
+}
+
+/*fonction pour afficher les différents évènements */
+
+function getAllEvents() {
+    global $db_connect;
+
+    $sql ="SELECT * FROM events";
+    $query = $db_connect->query($sql);
+    $events = $query->fetchAll(PDO::FETCH_ASSOC);
+    return $events;
+}
