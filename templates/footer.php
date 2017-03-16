bc045199b95bf6d003a7cda21a0f28f303603a0e
David Patissier <ddgraphdesign@gmail.com>
2017-03-16 20:14:40 +0100

    html de header et footer

---
+++ templates/footer.php
@@ -0,0 +1,10 @@
+ <footer id="footer" class="navbar-fixed-bottom">
+          &copy; <?= date('Y') ?> — Bonne Santé by SYDJ !
+        </footer>
+
+		<!-- fichiers JS -->
+		<!-- JQuery Bootstrap -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+    </body>
+</html>
