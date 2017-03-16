83b466cdba4a9ef8e0d2bfd8f3fb8f539176a380
Live session user <mint@etudiant.oclock.io>
2017-03-16 18:40:18 +0100

    init page accueil

--- templates/accueil.php
+++ templates/accueil.php
@@ -1,5 +1,43 @@
-<main id="diets" class="container">
 
+    <img src="" alt="">
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
+    <ul class="diets">
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+    </ul>
+    <ul class="events">
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+    </ul>
+
+
+
+
+
+<main id="diets" class="container">
+    <?php
+    foreach ($diets as $index => $diet) {
+        $link = "diets.php?index"
+        # code...
+    }
+
+
+</main>
+
+<main id="events" class="container">
+    <?php
+    foreach ($diets as $index => $diet) {
+        $link = "diets.php?index"
+        # code...
+    }
 
 
 </main>
