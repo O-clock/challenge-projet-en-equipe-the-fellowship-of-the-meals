67add454459fe32006fa9ad2b6fb7ec12fd30d6e
corke <corke.aj@gmail.com>
2017-03-17 13:23:00 +0100

    modif login

--- login.php
+++ login.php
@@ -4,7 +4,11 @@ require('inc/setup.php');
 
 
 //Récupération des données pour remplire le modèle
-getAllDiets();
+
+//On vérifie que l'utilisateur à envoyer le formulaire
+if(isset($_POST["login"])) {
+echo $_POST["login"];
+}
 
 
 include("templates/header.php");
