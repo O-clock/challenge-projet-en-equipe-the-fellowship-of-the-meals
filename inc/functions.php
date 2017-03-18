c0efe223bb1d6fb584b452dfb04c050181fd9322
corke <corke.aj@gmail.com>
2017-03-18 17:22:43 +0100

    fonction is Admin

--- inc/functions.php
+++ inc/functions.php
@@ -1,3 +1,10 @@
 <?php
 require('sessions.php');
 require('data.php');
+
+function isAdmin(){
+    $role = getRoleUser($_SESSION["user"]["id"]);
+    if($role == "admin"){
+        return $role;
+    }
+}
