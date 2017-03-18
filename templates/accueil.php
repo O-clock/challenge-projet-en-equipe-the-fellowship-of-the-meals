edba805d71d9cbfdc12eec22547bbd60fd2814eb
yvesdelepinay <yvesdelepinay@yahoo.fr>
2017-03-18 18:17:04 +0100

    ajout print_r accueil

--- templates/accueil.php
+++ templates/accueil.php
@@ -1,45 +1,16 @@
 
-    <img src="" alt="">
+
+    <img src="" alt="burger girl">
     <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
     </p>
-    <ul class="diets">
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-    </ul>
-    <ul class="events">
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-    </ul>
 
 
 
-
-
-<main id="diets" class="container">
+    <ul>
     <?php
-    foreach ($diets as $index => $diet) {
-        $link = "diets.php?index";
-        # code...
-    }
+    foreach ($diets as $index => $diet) {?>
+    <li><?php print_r($diet) ?></li>
+    <?php }
     ?>
-
-
-</main>
-
-<main id="events" class="container">
-    <?php
-    foreach ($diets as $index => $diet) {
-        $link = "diets.php?index";
-        # code...
-    }?>
-
-
-
-</main>
+    </ul>
