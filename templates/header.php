db32b4cbfd5b4f0253a25fa1ee63aa27a36c6eec
David Patissier <ddgraphdesign@gmail.com>
2017-03-18 17:04:27 +0100

    ajout font lobster

--- templates/header.php
+++ templates/header.php
@@ -6,6 +6,7 @@
         <link rel="stylesheet" href="css/bootstrap.min.css" />
         <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css?family=Roboto:400,300,100" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
         <link rel="stylesheet" href="css/style.css" />
     </head>
     <body>
