8cf9f805bc14de0cad1a53ae9b7ee9880febf351
David Patissier <ddgraphdesign@gmail.com>
2017-03-19 17:17:06 +0100

    MaJ nav header et acceuil + favicon

--- templates/accueil.php
+++ templates/accueil.php
@@ -1,8 +1,17 @@
 <main id="acceuil" class="container">
     <img src="images/photo-acceuil-SYDJ.jpg" alt="regime">
-    <p class="intro-acceuil">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    </p>
+    <div class="intro-acceuil">
+        <h1>Lorem ipsum dolor sit amet</h1>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <button type="button" name="connexion-facebook">Connectez vous avec <i class="fa fa-facebook-square" aria-hidden="true"></i></button>
+    </div>
+
+
+    <!-- Bouton Connection à Facebook
+    <!--<div class="fb-login-button" data-max-rows="1" data-size="xlarge" data-show-faces="false" data-auto-logout-link="true"></div>-->
+
     <!--<ul class="diets">
         <li></li>
         <li></li>
--- templates/header.php
+++ templates/header.php
@@ -2,6 +2,8 @@
 <html>
     <head>
         <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico" />
         <title>Bonne Santé</title>
         <link rel="stylesheet" href="css/bootstrap.min.css" />
         <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
@@ -11,27 +13,42 @@
     </head>
     <body>
       <header class="container">
-        <h1>Bonne Santé</h1>
-                <nav class="navbar navbar-default">
-			        <a class="home" href="#">Acceuil</a>
-                    <a class="admin" href="#">Admin</a>
-                    <a class="admin" href="#">Connexion</a>
-                    <a class="login" href="#">M'inscrire</a>
-			        <a class="profil" href="#">Profil</a>
-                    <a class="redact" href="#">Écrire un article</a>
-                    <a class="create" href="#">Créer un évènement</a>
-                        <div id="search">
-                            <form action="" id="form-search" method="GET">
-                                <input class="champ"
-                                        name="id"
-                                        type="text"
-                                        placeholder="Rechercher un évènement"
-                                        value =""
-                                        required />
-                                        <button id="acces-send">Rechercher</button>
-                                </form>
-                        </div>
-		        </nav>
-               <!-- Saisie barre de recherche -->
+	<nav class="navbar navbar-default">
+    <div id="menu">
+      <img href="index.php" class="site-logo" src="images/logo-bs.png" alt="Logo Bonne Santé">
+    <ul>
+      <li><a class="home" href="index.php">Acceuil</a></li>
 
-      </header>
+      <li><a class="admin" href="#">Admin</a>
+
+        <ul>
+          <li><a href="#">Acceuil Admin</a></li>
+          <li><a href="#">Créer une communauté</a></li>
+          <li><a href="#">Gestion des utilisateurs</a></li>
+          <li><a href="#">Valider des articles</a></li>
+        </ul>
+      </li>
+
+      <li><a class="admin" href="login.php">Connexion</a></li>
+      <li><a class="login" href="#">M'inscrire</a></li>
+
+
+      <li><a class="profil" href="#">Profil</a></li>
+      <li><a class="write" href="#">Ecrire un article</a></li>
+      <li><a class="create" href="#">Créer un évènement</a></li>
+
+    </ul>
+	<div id="search">
+              <form action="" id="form-search" method="GET">
+                <input class="champ"
+                name="id"
+                type="text"
+                placeholder="Rechercher un évènement"
+                value =""
+                required />
+                <button id="acces-send">Rechercher</button>
+              </form>
+            </div>
+  </div>
+  </nav>
+  </header>
