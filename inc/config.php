0a72b2a92516ec84fd62a7d7b1cb073be9df8249
David Patissier <ddgraphdesign@gmail.com>
2017-03-20 17:48:31 +0100

    reglage conflitn 2 le retour

--- inc/config.php
+++ inc/config.php
@@ -1,13 +1,6 @@
 <?php
 
-<<<<<<< HEAD
 $host = "localhost";
 $user = "meal";
 $pass = "meal";
 $dbname = "meal";
-=======
-  $host = "localhost";
-  $user = "meal";
-  $pass = "meal";
-  $dbname = "meal";
->>>>>>> 13d8ff1897a0b3debc2d54d1c709b9950dbe23a8
